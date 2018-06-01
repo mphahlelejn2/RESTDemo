4d88a02514dcef739f956a25d16ae2ba95b181a6
Jeffrey.Mphahlele <Jeffrey.mphahlele@bme.co.za>
2018-06-01 17:51:18 +0200

    testing getColorsList

--- app/src/test/java/com/kamo/restdemo/dao/RepositoryImpTest.java
+++ app/src/test/java/com/kamo/restdemo/dao/RepositoryImpTest.java
@@ -1,11 +1,16 @@
 package com.kamo.restdemo.dao;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.kamo.restdemo.color.Color;
+import com.kamo.restdemo.user.User;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,82 +25,52 @@ import static org.mockito.Mockito.when;
 /**
  * Created by Jeffrey.Mphahlele on 2018/05/29.
  */
-public class RepositoryImpTest {
+public class RepositoryImpTest{
 
     @Mock
     private APIClient client;
-
     private IRepository  repository;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         repository=new RepositoryImp(client);
     }
 
-
-   /* public void loadListOfColors_Results_Ok() {
-        List<Color> list=getListOfColors();
-        TestObserver<List<Color>> observer = new TestObserver<>();
-        //TestSubscriber<List<Color>> wordListTestSubscriber=new TestSubscriber<>();
-        //Given
-        // Maybe<List<Color>> maybe=repository.getColorList();
-        //maybe.subscribe(observer);
-        when(repository.getColorList()).thenReturn(Maybe.just(list));
-
-        // when:
-        repository.getColorList().subscribe(observer);
-
-        presenter.loadListOfColors();
-
-        //then
-        observer.assertNoErrors();
-        observer.assertSubscribed();
-        observer.assertComplete();
-
-        verify(view).initLoadProgressDialog();
-        verify(view,never()).errorLoadingColor();
-        verify(view).loadAdaptor(list);
-        verify(view,never()).colorListEmpty();
-    }*/
-
-    @Test
-    public void getColorList() throws Exception {
+   @Test
+    public void getColorList_Results_Ok() {
+       
         List<Color> list= getListOfColors();
         when(client.getColorList()).thenReturn(Maybe.just(list));
 
         //When
         TestObserver<List<Color>> observer = new TestObserver<>();
         repository.getColorList().subscribe(observer);
+        //then
 
         observer.assertNoErrors();
         observer.assertSubscribed();
         observer.assertComplete();
         observer.awaitTerminalEvent();
 
+       List<Color> colorList=  observer.values().get(0);
+       // Color c= (Color) observer.getEvents().get(1);
+         Assert.assertEquals(colorList.get(0), list.get(0));
+         Assert.assertEquals(colorList.get(1).getThumbnailUrl(), list.get(1).getThumbnailUrl());
 
-       // List<List<Color>> onNextEvents = observer.getOnNextEvents();
-       // List<Color> users = onNextEvents.get(0);
-       // Assert.assertEquals(USER_LOGIN_RIGGAROO, users.get(0).getLogin());
-       // Assert.assertEquals(USER_LOGIN_2_REBECCA, users.get(1).getLogin());
-       // verify(githubUserRestService).searchGithubUsers(USER_LOGIN_RIGGAROO);
-        //verify(githubUserRestService).getUser(USER_LOGIN_RIGGAROO);
-        //verify(githubUserRestService).getUser(USER_LOGIN_2_REBECCA);
     }
 
     @Test
-    public void getUserList() throws Exception {
+    public void getUserList() {
+
     }
 
     @Test
-    public void getColor() throws Exception {
+    public void getColor() {
+
     }
 
     private List<Color> getListOfColors() {
-        Color c1=new Color();
-        Color c2=new Color();
-        List<Color> list =new ArrayList<>();
-        list.add(c1);
-        list.add(c2);
-        return list;
+        return  new Gson().fromJson(TestData.jsonListOfColors, new TypeToken<List<Color>>(){}.getType());
     }
 }
