d791df836c7ce4e120dfd8e3464a1017c815b391
Jeffrey.Mphahlele <mphahlelejn2@gmail.com>
2018-07-19 15:31:53 +0200

    fixing UI test

--- app/src/androidTest/java/com/kamo/restdemo/ListOfUsersFragmentTest.java
+++ app/src/androidTest/java/com/kamo/restdemo/ListOfUsersFragmentTest.java
@@ -1,28 +1,18 @@
 package com.kamo.restdemo;
-
 import android.os.SystemClock;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-
 import com.kamo.restdemo.main.MainActivity;
-
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -41,6 +31,7 @@ public class ListOfUsersFragmentTest {
 
     @Test
     public void checkTextDisplayedInDynamicallyCreatedFragment() {
+
         Espresso.onView(withId(R.id.viewpager)).perform(swipeLeft());
         onView(withId(R.id.user_recycler_view))
                 .perform(RecyclerViewActions.scrollToPosition(1));
